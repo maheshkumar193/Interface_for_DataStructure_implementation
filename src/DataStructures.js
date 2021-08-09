5945440dd63f0c7b6095456cb6eca9c39eb7ada7
maheshkumar193 <maheshbodhan19@gmail.com>
2021-08-09 17:49:22 +0530

    final version

--- src/DataStructures.js
+++ src/DataStructures.js
@@ -1,87 +1,86 @@
 const dataStructures = [
-    {
-      name: "AVL Tree",
-      imgURL: "https://static.javatpoint.com/ds/images/avl-tree.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/AVL.cpp"
-    },
-    {
-      name: "Red Black Tree",
-      imgURL:
-        "https://shunsvineyard.info/wp-content/uploads/2021/04/red_black_tree-theory.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/redBlackTree.cpp"
-    },
-    {
-      name: "Segment Tree",
-      imgURL:
-        "https://java2blog.com/wp-content/webpc-passthru.php?src=https://java2blog.com/wp-content/uploads/2018/10/SegmentTreeTC.jpg&nocache=1",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/SegmentTree.cpp"
-    },
-    {
-      name: "Max Heap",
-      imgURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ApFLTfZ55sFJ4Eyi9RP3-xicrSCpJz6hBtnyLa6i293W8Wcm5aX1VaBrhloWg8yoXqJcG4NcVvuVxg&usqp=CAU",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/maxHeap.cpp"
-    },
-    {
-      name: "Min Heap",
-      imgURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7qMmXhwJ2bWtR67LNK7JvpQUQFbO-WVqCaXUDc8h2RhMZr7e6VE0Fi_OuLL_h0LN1GzmkL_2U02v96A&usqp=CAU",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/minHeap.cpp"
-    },
-    {
-      name: "Queue",
-      imgURL:
-        "https://cdn.journaldev.com/wp-content/uploads/2018/06/swift-queue-flow.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/queue.cpp"
-    },
-    {
-      name: "Circular Queue",
-      imgURL:
-        "https://static.packt-cdn.com/products/9781786463890/graphics/image_04_006.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/Circularqueue.cpp"
-    },
-    {
-      name: "LinkedList Queue",
-      imgURL:
-        "https://static.javatpoint.com/ds/images/linked-list-implementation-of-queue.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/LinkedListqueue.cpp"
-    },
-    {
-      name: "stack",
-      imgURL:
-        "https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/06/pictorial-representation-of-stack.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/stacks.cpp"
-    },
-    {
-      name: "LinkedList Stack",
-      imgURL: "https://media.geeksforgeeks.org/wp-content/uploads/2-260.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/LinkedListstacks.cpp"
-    },
-    {
-      name: "vector",
-      imgURL:
-        "https://www.edureka.co/blog/wp-content/uploads/2019/08/vectors-in-c-528x130.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/vector.cpp"
-    },
-    {
-      name: "deque",
-      imgURL:
-        "https://cdn.programiz.com/sites/tutorial2program/files/deque.png",
-      codeLink:
-        "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/vector.cpp"
-    }
-  ];
-  
-  export default dataStructures;
-  
+  {
+    name: "AVL Tree",
+    imgURL: "https://static.javatpoint.com/ds/images/avl-tree.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/AVL.cpp"
+  },
+  {
+    name: "Red Black Tree",
+    imgURL:
+      "https://shunsvineyard.info/wp-content/uploads/2021/04/red_black_tree-theory.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/redBlackTree.cpp"
+  },
+  {
+    name: "Segment Tree",
+    imgURL:
+      "https://java2blog.com/wp-content/webpc-passthru.php?src=https://java2blog.com/wp-content/uploads/2018/10/SegmentTreeTC.jpg&nocache=1",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/SegmentTree.cpp"
+  },
+  {
+    name: "Max Heap",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ApFLTfZ55sFJ4Eyi9RP3-xicrSCpJz6hBtnyLa6i293W8Wcm5aX1VaBrhloWg8yoXqJcG4NcVvuVxg&usqp=CAU",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/maxHeap.cpp"
+  },
+  {
+    name: "Min Heap",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7qMmXhwJ2bWtR67LNK7JvpQUQFbO-WVqCaXUDc8h2RhMZr7e6VE0Fi_OuLL_h0LN1GzmkL_2U02v96A&usqp=CAU",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/minHeap.cpp"
+  },
+  {
+    name: "Queue",
+    imgURL:
+      "https://cdn.journaldev.com/wp-content/uploads/2018/06/swift-queue-flow.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/queue.cpp"
+  },
+  {
+    name: "Circular Queue",
+    imgURL:
+      "https://static.packt-cdn.com/products/9781786463890/graphics/image_04_006.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/Circularqueue.cpp"
+  },
+  {
+    name: "LinkedList Queue",
+    imgURL:
+      "https://static.javatpoint.com/ds/images/linked-list-implementation-of-queue.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/LinkedListqueue.cpp"
+  },
+  {
+    name: "stack",
+    imgURL:
+      "https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/06/pictorial-representation-of-stack.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/stacks.cpp"
+  },
+  {
+    name: "LinkedList Stack",
+    imgURL: "https://media.geeksforgeeks.org/wp-content/uploads/2-260.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/LinkedListstacks.cpp"
+  },
+  {
+    name: "vector",
+    imgURL:
+      "https://www.programmersought.com/images/193/4c2fafee4e63926ffa94ec243652af39.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/vector.cpp"
+  },
+  {
+    name: "deque",
+    imgURL:
+      "https://www.javatpoint.com/post/resources/article-images/c++-deque/cpp-deque.png",
+    codeLink:
+      "https://github.com/maheshkumar193/Data-Structures-Implementation/blob/master/vector.cpp"
+  }
+];
+
+export default dataStructures;
